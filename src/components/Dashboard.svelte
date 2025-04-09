ecbf56b9810957752726991e84efc51f11b9a6cf
Shivali-Singireddy <shivali5@mit.edu>
2025-04-09 13:32:54 -0400

    Update Dashboard.svelte

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -36,7 +36,7 @@
     on:selectNeighborhood={handleSelectNeighborhood}
     on:resetNeighborhood={handleResetNeighborhood}
   />
-  <div style="margin-left: 20px; display: flex; flex-direction: column; align-items: center;">
+  <div style="margin-left: 40px; display: flex; flex-direction: column; align-items: center;">
     {#if selectedDetails}
       <PieChart details={selectedDetails} />
     {/if}
